464ea40952cc8b3df2c346b6c6287bb158f8d6bf
sidharth nayyar <sidnayyar1@gmail.com>
2019-11-10 22:38:00 -0500

    [sidharth] do try statement to check whether data is saved in core data or not.

--- Notes/ViewController.swift
+++ Notes/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 import CoreData
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate,UINavigationControllerDelegate,UIImagePickerControllerDelegate,UITextViewDelegate {
 
 
     @IBOutlet weak var noteInfoView: UIView!
@@ -36,6 +36,53 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //this is to load data
+        if let note = note{
+            noteNameLabel.text = note.noteName
+            noteDescriptionLabel.text = note.noteDescription
+            noteImageView.image = UIImage(data: note.noteImage! as Data)
+            
+        }
+        if noteNameLabel.text != "" {
+            isEditing = true
+        }
+        
+        //delegates
+        noteNameLabel.delegate = self
+        noteDescriptionLabel.delegate = self
+        
+        //style
+        noteInfoView.layer.shadowColor =  UIColor(red:0/255.0, green:0/255.0, blue:0/255.0, alpha: 1.0).cgColor
+        noteInfoView.layer.shadowOffset = CGSize(width: 0.75, height: 0.75)
+        noteInfoView.layer.shadowRadius = 1.5
+        noteInfoView.layer.shadowOpacity = 0.2
+        noteInfoView.layer.cornerRadius = 2
+        
+        noteImageViewView.layer.shadowColor =  UIColor(red:0/255.0, green:0/255.0, blue:0/255.0, alpha: 1.0).cgColor
+        noteImageViewView.layer.shadowOffset = CGSize(width: 0.75, height: 0.75)
+        noteImageViewView.layer.shadowRadius = 1.5
+        noteImageViewView.layer.shadowOpacity = 0.2
+        noteImageViewView.layer.cornerRadius = 2
+        
+        noteImageView.layer.cornerRadius = 2
+        noteNameLabel.setBottomborder()
+        
+    }
+    
+    
+    func saveCoreData(completion: @escaping () ->Void){
+        manageObjectContext?.perform {
+            do{
+                try self.manageObjectContext?.save()
+                completion()
+                print("note save to core data")
+                
+            }
+            catch let error {
+                print("could not save note to core data")
+            }
+        }
+        
     }
 
 
