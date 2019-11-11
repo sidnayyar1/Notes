a2c2e5b74b497e9e2fca0ad6b64b3a3f2a8ea985
sidharth nayyar <sidnayyar1@gmail.com>
2019-11-10 21:39:22 -0500

    [sidharth] added new table view cell

---
+++ Notes/noteTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  noteTableViewCell.swift
+//  Notes
+//
+//  Created by Sidharth Nayyar on 11/10/19.
+//  Copyright © 2019 Sidharth Nayyar. All rights reserved.
+//
+
+import UIKit
+
+class noteTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
