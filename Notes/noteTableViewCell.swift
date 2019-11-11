503a226c50e594a285fa9342137895478ec36170
sidharth nayyar <sidnayyar1@gmail.com>
2019-11-10 22:03:37 -0500

    [sidharth] added configure cell function.

--- Notes/noteTableViewCell.swift
+++ Notes/noteTableViewCell.swift
@@ -17,13 +17,23 @@ class noteTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-      
+        //style
+        shadowView.layer.shadowColor = UIColor(red: 245.0/255.0, green: 79.0/255.0, blue: 80.0/255.0, alpha: 1.0).cgColor
+        shadowView.layer.shadowOffset = CGSize(width: 0.75, height: 0.75)
+        shadowView.layer.shadowRadius = 1.5
+        shadowView.layer.shadowOpacity = 0.2// 80% transparent
+        shadowView.layer.cornerRadius = 2
+        noteImage.layer.cornerRadius = 2
+
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
     }
+    func configureCell(note: Note){
+        self.noteNameLabel.text = note.noteName?.uppercased()//upper case is not necessory but looks good as title is always in uppercase
+    }
+    
 
 }
